d9a8479f2084e4c99e2ae2b80db33d349f9963e9
zscaiosi <zscaio.si@gmail.com>
2018-02-03 10:29:30 -0200

    [ChatRoom e Bootstrap] Criando fluxo de cadastro e login + Bootstrap via npm;

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,8 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import Base from './Base';
 import registerServiceWorker from './registerServiceWorker';
-import './css/index.css';
 import { BrowserRouter } from 'react-router-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <BrowserRouter>
